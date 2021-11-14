46c61caad6a5b7b6bd6c92321143830703f236ec
ggmu1986 <gt2186@gmail.com>
2021-11-14 16:28:44 -0500

    Finished User auth, started validations

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 
 mongoose
@@ -12,7 +13,9 @@ mongoose
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
+// app.get("/", (req, res) => res.send("Hello World"));
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
 
 router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
@@ -46,12 +49,32 @@ router.post('/login', (req, res) => {
       bcrypt.compare(password, user.password)
         .then(isMatch => {
           if (isMatch) {
-            res.json({msg: 'Success'});
+            const payload = {id: user.id, handle: user.handle};
+            // res.json({msg: 'Success'});
+            jwt.sign(
+              payload,
+              keys.secretOrKey,
+              // Tell the key to expire in one hour
+              {expiresIn: 3600},
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: 'Bearer ' + token
+                });
+              });
           } else {
             return res.status(400).json({password: 'Incorrect password'});
           }
         })
     })
+})
+
+router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
+  res.json({
+    id: req.user.id,
+    handle: req.user.handle,
+    email: req.user.email
+  });
 })
 
 module.exports = router;
